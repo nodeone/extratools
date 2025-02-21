0131754b3af1c24366d568d6e6992fa2ec810ff6
Mikke Schirén <mikke.schiren@digitalist.com>
2025-02-21 13:55:53 +0100

    update dependencies

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd /home/runner/work/Matomo-Plugin-ExtraTools/Matomo-Plugin-ExtraTools/plugins/ExtraTools
-composer install --no-dev
 cd /home/runner/work/Matomo-Plugin-ExtraTools/Matomo-Plugin-ExtraTools/
 composer install
