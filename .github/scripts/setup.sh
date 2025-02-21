3dc9ba6bd6d5a1258419e7fd6c925bacd5d879bb
Mikke Schirén <mikke.schiren@digitalist.com>
2025-02-21 13:32:58 +0100

    add another composer step

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -2,3 +2,5 @@
 
 cd /home/runner/work/Matomo-Plugin-ExtraTools/Matomo-Plugin-ExtraTools/plugins/ExtraTools
 composer install --no-dev
+cd /home/runner/work/Matomo-Plugin-ExtraTools/Matomo-Plugin-ExtraTools/
+composer install
